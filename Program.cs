f7772464b9c0f91231620fdfea81a990e15ad89e
Che Sergiu <37384588+CHESERGIU@users.noreply.github.com>
2020-07-03 10:45:09 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace App36
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool found = true;
+            bool check = false;
+            int count = 0;
+            string input = Console.ReadLine();
+            int number = int.Parse(Console.ReadLine());
+            while (count != number)
+            {
+                if (Console.ReadLine() == input)
+                {
+                    Console.WriteLine(found);
+                    check = true;
+                    break;
+                }
+                count++;
+            }
+            if(check == false)
+                Console.WriteLine(!found);
+        }
+
+    }
+}
